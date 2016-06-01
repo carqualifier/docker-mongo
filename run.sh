b609e7413df353f8dd5203c91c2174d252b325df
Christopher Willard <cwillard@carqualifier.com>
2016-06-01 11:32:03 -0400

    Update

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+DB_PATH="server_{$UUID}"
+mkdir -p $DB_PATH
+
 echo $MONGODB_KEYFILE > /opt/rancher/sec/mongodb-keyfile
 
-mongod --keyFile /opt/rancher/sec/mongodb-keyfile --quiet --profile 0 --replSet $MONGODB_REPLSET_NAME --storageEngine wiredTiger
+mongod --dbpath /data/db/$DB_PATH --keyFile /opt/rancher/sec/mongodb-keyfile --quiet --profile 0 --replSet $MONGODB_REPLSET_NAME --storageEngine wiredTiger

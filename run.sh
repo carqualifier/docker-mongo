d791c677e41e873a362c97d08b3bf75604d07602
Christopher Willard <cwillard@carqualifier.com>
2016-06-02 11:40:50 -0400

    Update

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ mkdir -p $DB_PATH
 
 echo $MONGODB_KEYFILE > /opt/rancher/sec/mongodb-keyfile
 
-mongod --dbpath $DB_PATH --keyFile /opt/rancher/sec/mongodb-keyfile --quiet --profile 0 --replSet $MONGODB_REPLSET_NAME --storageEngine wiredTiger
+mongod --dbpath $DB_PATH --keyFile /opt/rancher/sec/mongodb-keyfile --quiet --profile 0 --replSet $MONGODB_REPLSET_NAME --wiredTigerCacheSizeGB 4 --storageEngine wiredTiger
